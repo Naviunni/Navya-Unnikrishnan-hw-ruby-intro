2eeeca6d53e6425cb13e23d0120f41603383e2b0
Navya Unnikrishnan <naviunni@gmail.com>
2024-09-03 14:46:27 -0500

    getters and setters of BookInStock implemented

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -21,21 +21,21 @@ def max_2_sum(arr)
   if arr.at(0) > arr.at(1)
     largest = arr.at(0)
     second = arr.at(1)
-  else 
+  else
     largest = arr.at(1)
     second = arr.at(0)
   end
 
-  for i in 2..n-1
+  for i in 2..n - 1
     if arr.at(i) >= largest
       second = largest
       largest = arr.at(i)
-    elsif arr.at(i) >= second and arr.at(i) != largest
+    elsif arr.at(i) >= second && arr.at(i) != largest
       second = arr.at(i)
     end
   end
 
-  return (largest+second)
+  largest+second
 end
 
 def sum_to_n?(arr, number)
@@ -46,38 +46,51 @@ def sum_to_n?(arr, number)
 
   set = Set.new
   for i in arr
-    if set.include?(number-i)
+    if set.include?(number - i)
       return true
     end
+
     set.add?(i)
   end
-  return false
+  false
 end
 
 # Part 2
 
 def hello(name)
-  return( "Hello, #{name}" )
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
-  if string.length == 0
+  if string.empty?
     return false
   end
-  return ( ((string[0] =~ /[aeiou]/i ) == nil) and ((string[0] =~ /[a-z]/i )!= nil) )
+((string[0] =~ /[aeiou]/i ).nil?) and (!(string[0] =~ /[a-z]/i ).nil?)
   
 end
 
 def binary_multiple_of_4?(string)
-  if string == '0'
-    return true
-  end
-  return (string =~ /^[01]*00$/) !=nil
+  string == '0' || !(string =~ /^[01]*00$/).nil?
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+  def isbn
+    @isbn
+  end
+  def price
+    @price
+  end
+  def isbn=(isbn)
+    @isbn=isbn
+  end
+  def price=(price)
+    @price=price
+  end
 end
